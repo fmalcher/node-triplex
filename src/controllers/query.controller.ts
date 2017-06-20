83d82dd5b5a2ba6227584a61d5e4aedef5c6096f
Robert Weiße <github@robert-weisse.de>
2017-06-20 14:26:24 +0200

    Error status codes.

--- src/controllers/query.controller.ts
+++ src/controllers/query.controller.ts
@@ -5,14 +5,9 @@ export class QueryController {
 
     processQuery(req, res, next) {
         RequestService.readFromUri(req.body)
-            .catch(error => res.writeHead(error))
-                .then(content => { if (content)
-                    return RequestService.findDataTypes(content) })
-                    .catch(error => res.writeHead(error))
-                        .then(dataTypes => { if (dataTypes)
-                            return RequestService.generateResponse(dataTypes, req.body) })
-                            .catch(error => res.writeHead(error))
-                                .then(response => { if (response)
-                                    return res.json(response) });
+        .then(content => { if (content) return RequestService.findDataTypes(content) })
+        .then(dataTypes => { if (dataTypes) return RequestService.generateResponse(dataTypes, req.body) })
+        .then(response => { if (response) return res.json(response) })
+        .catch(error => res.send(error));
     }
 }
